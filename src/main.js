0fa3f0a3c564e73eea3aa3abcf86358908ace8cc
Jwade1781 <jwade5219@gmail.com>
2022-09-13 23:25:31 -0400

    Player functionality added, win condition working

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+let gameControl;
+
 const createMainHeader = () => {
     const headerDiv = document.createElement("div");
     const headerText = document.createElement("h4");
@@ -45,21 +47,40 @@ const createGameBoard = () => {
     const gameBoard = document.createElement("div");
     gameBoard.id = "game";
 
+    let i = 0;
     for (let col = 0; col < boardWidth; col++) {
         for (let row = 0; row < boardLength; row++) {
             const gameCell = document.createElement("div");
+            const cellNum = i;
+            gameCell.addEventListener("click", () => {
+                if (gameControl.getGameActive() && !(gameCell.firstChild)) {
+                    const playerMoveHeader = document.createElement("h1");
+                    const playerMove = gameControl.move(cellNum)
+                    if (playerMove) {
+                        playerMoveHeader.textContent = playerMove;
+                        gameCell.appendChild(playerMoveHeader);
+                    }
+                }
+            })
+
             const cellClassList = ["row" + (row % boardWidth), "col" + (col % boardLength), "cell", "invertColor", "darkHover"];
             for (const className of cellClassList)
                 gameCell.classList.add(className);
             gameBoard.appendChild(gameCell);
+            i++;
         }
     }
 
     const infoPanel = document.createElement("div");
     const infoPanelText = document.createElement("h3");
     infoPanelText.textContent = "Start Game";
-    infoPanel.id = "infoPanel";
+    infoPanel.addEventListener("click", () => {
+        if (!gameControl.getGameActive()) gameControl.startGame();
+    })
+
+    infoPanel.id = "startGameButton";
     infoPanel.classList.add("invertColor");
+    infoPanel.classList.add("infoPanel");
     infoPanel.appendChild(infoPanelText);
     gameBoard.appendChild(infoPanel);
     return gameBoard;
@@ -76,14 +97,105 @@ const createStartupDom = () => {
         mainDiv.appendChild(element);
 
     document.body.appendChild(mainDiv);
-    return {"createPass" : true, "mainDiv" : mainDiv};
+    return { "createPass": true, "mainDiv": mainDiv };
+}
+
+const player = (playerNumber, moveChar, botBool) => {
+    let moveSet = [];
+    const addMove = (cellNum) => moveSet.push(cellNum);
+    const getMoves = () => moveSet;
+    const getPlayerNumber = () => playerNumber;
+    const getMoveChar = () => moveChar;
+    const getBotBool = () => botBool;
+    return { getPlayerNumber, getMoveChar, getMoves, addMove, getBotBool };
+}
+
+const players = () => {
+    const getPlayer1 = player(1, "X", false);
+    const getPlayer2 = player(2, "O", false);
+    return { getPlayer1, getPlayer2 };
+}
+
+const gameController = () => {
+    let playerList;
+    let currentPlayer;
+    let gameActive;
+
+    const getCurrentPlayer = () => currentPlayer;
+    const toggleCurrentPlayer = () => currentPlayer === playerList.getPlayer1 ? currentPlayer = playerList.getPlayer2 : currentPlayer = playerList.getPlayer1;
+
+    const getGameActive = () => gameActive;
+    const toggleGameActive = () => { gameActive ? gameActive = false : gameActive = true };
+
+    const writeGameButton = (string) => document.querySelector("#startGameButton").firstChild.textContent = string;
+    const writePlayerTurn = () => writeGameButton(`Player ${currentPlayer.getPlayerNumber()}'s Turn:  ${currentPlayer.getMoveChar()}`);
+    const writePlayerWin = () => {
+        window.alert(`Player ${currentPlayer.getPlayerNumber()} Wins!`);
+        writeGameButton("Start Game");
+    }
+    const reset = () => {
+        playerList = players();
+        currentPlayer = playerList.getPlayer1;
+        gameActive = false;
+
+        let cells = document.querySelectorAll(".cell");
+        for (cell of cells) {
+            if (cell.lastElementChild)
+                cell.removeChild(cell.lastElementChild);
+        }
+    }
+    const checkWin = () => {
+        const winConditions = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  // Horizontal
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Vertical
+            [0, 4, 8], [2, 4, 6]             // Diagonal
+        ]
+
+        for (let i = 0; i < winConditions.length; i++) {
+            for (let j = 0; j < winConditions[i].length; j++) {
+                if (!(currentPlayer.getMoves().includes(winConditions[i][j])))
+                    break;
+
+                if (j + 1 === winConditions[i].length)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    const move = (cellNum) => {
+        const playedMove = currentPlayer.getMoveChar();
+        currentPlayer.addMove(cellNum);
+        if (checkWin()) {
+            toggleGameActive();
+            writePlayerWin();
+            reset();
+            return false;
+        }
+
+        else {
+            toggleCurrentPlayer();
+            writePlayerTurn();
+            return playedMove;
+        }
+    }
+
+    const startGame = () => {
+        reset();
+        toggleGameActive();
+        writePlayerTurn();
+    }
+
+    return { getGameActive, getCurrentPlayer, move, startGame };
 }
 
 const main = () => {
+    gameControl = gameController();
     const startUpDom = createStartupDom();
 
     if (!startUpDom["createPass"])
         throw 'Dom was not successfully created';
+
 }
 
 window.addEventListener("load", () => main());
